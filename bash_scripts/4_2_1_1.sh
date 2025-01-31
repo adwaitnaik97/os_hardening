46e11686b6903de7b2e180f290b590ee42681c26
adwaitnaik97 <adwait.aidrivers.ai>
2025-01-31 10:13:59 +0000

    Added some screenshots

--- bash_scripts/4_2_1_1.sh
+++ bash_scripts/4_2_1_1.sh
@@ -10,8 +10,31 @@ fi
 echo "Verifying if either rsyslog or syslog-ng is installed..."
 if dpkg -s rsyslog &> /dev/null || dpkg -s syslog-ng &> /dev/null; then
     echo "Either rsyslog or syslog-ng is installed."
+    # Prompt the user to set up rsyslog
+    read -p "Do you want to configure rsyslog on your system? (yes/y to proceed): " user_input
+    if [[ "$user_input" == "yes" || "$user_input" == "y" ]]; then
+
+        # Open the webpage for further setup instructions
+        echo "Opening the webpage for rsyslog setup instructions..."
+        
+        # Check for available browsers and use the first one found
+        if command -v firefox &> /dev/null; then
+            sudo -u "$SUDO_USER" firefox "https://www.manageengine.com/products/eventlog/logging-guide/syslog/configuring-ubuntu-lts-as-rsyslog-server.html"
+        elif command -v google-chrome &> /dev/null; then
+            sudo -u "$SUDO_USER" google-chrome "https://www.manageengine.com/products/eventlog/logging-guide/syslog/configuring-ubuntu-lts-as-rsyslog-server.html"
+        elif command -v chromium-browser &> /dev/null; then
+            sudo -u "$SUDO_USER" chromium-browser "https://www.manageengine.com/products/eventlog/logging-guide/syslog/configuring-ubuntu-lts-as-rsyslog-server.html"
+        elif command -v xdg-open &> /dev/null; then
+            sudo -u "$SUDO_USER" xdg-open "https://www.manageengine.com/products/eventlog/logging-guide/syslog/configuring-ubuntu-lts-as-rsyslog-server.html"
+        else
+            echo "No supported web browser found. Please open the following link manually:"
+            echo "https://www.manageengine.com/products/eventlog/logging-guide/syslog/configuring-ubuntu-lts-as-rsyslog-server.html"
+        fi
+    else
+        echo "rsyslog setup was not initiated."
+    fi
 else
-    echo "Neither rsyslog nor syslog-ng is installed. Installing rsyslog..."
-    sudo apt install -y rsyslog
-    echo "rsyslog has been installed."
+    echo "Neither rsyslog nor syslog-ng is installed."
+    echo "Installing rsyslog...."
+    sudo apt install rsyslog
 fi
